e46ed4a28c2d4b2f67f0d6473364a2c2d047ec48
Suzanne Leber <79wabvlw1dt7@opayq.com>
2018-06-08 11:06:05 -0700

    One small change

--- suzanne-sara/scripts/article.js
+++ suzanne-sara/scripts/article.js
@@ -18,6 +18,7 @@ Article.prototype.toHtml = function() {
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
 
 
