b2b3ba459d190312c8d0a3db01f752e13840cbc1
Suzanne Leber <79wabvlw1dt7@opayq.com>
2018-06-08 11:58:10 -0700

    Used handlebars to compile blog articles

--- suzanne-sara/scripts/article.js
+++ suzanne-sara/scripts/article.js
@@ -12,7 +12,7 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  // DONE: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
@@ -31,8 +31,11 @@ Article.prototype.toHtml = function() {
   //   this.publishStatus = '(draft)';
   // }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  // DONE: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
+  let source = $('#article-template').html();
+  let templateRender = Handlebars.compile(source);
+  return templateRender(this);
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
